2a4891f9bb7ee2f12d1331dcd135133ae5ce9d3b
iam-anish <anumathakiya073@gmail.com>
2024-01-08 17:47:47 +0530

    Initial Task Done

---
+++ src/test/java/com/resotechtaskmang/ResoTechTaskMangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resotechtaskmang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResoTechTaskMangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
